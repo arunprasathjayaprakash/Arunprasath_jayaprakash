9f316d48ebaaced19e49018c87d322fa278e4ade
arunJP <arun826.jp@gmail.com>
2023-10-27 12:19:37 -0700

    Added start page and custom css

--- arunprasath_jayaprakash/website/urls.py
+++ arunprasath_jayaprakash/website/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home , index
+from .views import home , index , create_contact
 
 urlpatterns = [
     path('', index , name='website_home'),
+    path('create',create_contact , name='create_contact_page' )
 ]
--- arunprasath_jayaprakash/website/views.py
+++ arunprasath_jayaprakash/website/views.py
@@ -8,7 +8,10 @@ def index(request):
         'Project' : 'Django'
     }
 
-    return render(request , 'base.html' ,context=content)
+    return render(request , 'base.html' , {'render':content})
+
+def create_contact(request):
+    return render(request,'new_contact_page.html')
 
 def home(request):
     return HttpResponse("<h1>home from pycharm</h2>")
